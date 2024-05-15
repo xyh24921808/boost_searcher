e4441b55ca0799bca46af0e47bda4a5ac7987fd2
Wind_shaow <FX>
2024-05-15 16:29:33 +0800

    updata

--- comm/util.hpp
+++ comm/util.hpp
@@ -37,11 +37,11 @@ public:
     }
 };
 
-const char *const DICT_PATH = "./dict/jieba.dict.utf8";
-const char *const HMM_PATH = "./dict/hmm_model.utf8";
-const char *const USER_DICT_PATH = "./dict/user.dict.utf8";
-const char *const IDF_PATH = "./dict/idf.utf8";
-const char *const STOP_WORD_PATH = "./dict/stop_words.utf8";
+const char *const DICT_PATH = "./comm/cppjieba/dict/jieba.dict.utf8";
+const char *const HMM_PATH = "./comm/cppjieba/dict/hmm_model.utf8";
+const char *const USER_DICT_PATH = "./comm/cppjieba/dict/user.dict.utf8";
+const char *const IDF_PATH = "./comm/cppjieba/dict/idf.utf8";
+const char *const STOP_WORD_PATH = "./comm/cppjieba/dict/stop_words.utf8";
 
 class Jieba_util
 {
