9adf269b43c3bac77503a26d027cc67e42c7f7f4
Wind_shaow <FX>
2024-05-17 17:13:02 +0800

    updata

--- server.cpp
+++ server.cpp
@@ -6,6 +6,10 @@ const string root_path = "./wwwroot";
 
 const string pre_path="/BS";
 
+int port=8081;
+
+
+Searcher*sc_ptr=nullptr;
 
 void Http_get_index(const httplib::Request &req, httplib::Response &rsp)
 {
@@ -21,31 +25,39 @@ void Http_get_index(const httplib::Request &req, httplib::Response &rsp)
     }
 }
 
-int main()
+void Http_search_post(const httplib::Request &req, httplib::Response &rsp)
 {
+    if(!req.has_param("word"))
+    {
+        rsp.set_content("必须要有搜索关键字!", "text/plain; charset=utf-8");
+        return;
+    }
+    std::string word = req.get_param_value("word");
+    
+    std::string json_string;
+    sc_ptr->Search(word,json_string);
+    rsp.set_content(json_string, "application/json");
+}
+
+int main(int argnum, char *argvs[])
+{
+    if(argnum==2)
+    {
+        port=atoi(argvs[1]);
+    }
+
     Searcher search;
     search.InitSearcher(input);
 
+    sc_ptr=&search;
     httplib::Server svr;
     
     svr.Get(pre_path,Http_get_index);
     svr.Get(pre_path+"/",Http_get_index);
 
-    svr.Get(pre_path+"/s", [&search](const httplib::Request &req, httplib::Response &rsp)
-            {
-            if(!req.has_param("word"))
-            {
-                rsp.set_content("必须要有搜索关键字!", "text/plain; charset=utf-8");
-                return;
-            }
-            std::string word = req.get_param_value("word");
-            
-
-            std::string json_string;
-            search.Search(word,json_string);
-            rsp.set_content(json_string, "application/json"); });
+    svr.Get(pre_path+"/s",Http_search_post);
 
     //svr.set_base_dir(root_path.c_str());
-    svr.listen("127.0.0.1", 8080);
+    svr.listen("127.0.0.1",port);
     return 0;
 }
